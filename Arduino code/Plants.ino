942c5a151ed5543438a1ea4b3aed2d98523e4aca
Horváth Patrik <41586252+TheRealPati@users.noreply.github.com>
2019-03-12 11:46:11 +0100

    Fixed network communication, JSON format

--- Arduino code/Plants.ino
+++ Arduino code/Plants.ino
@@ -1,14 +1,16 @@
 #include <SPI.h>
-#include <Ethernet2.h>
-#include <EthernetUdp2.h> 
+#include <Ethernet.h>
 
-//Local addresses
-byte mac[] = {0x90, 0x91, 0x92, 0x93, 0x94, 0x95};
-IPAddress ip(169, 254, 72, 169);
+//Local MAC address
+byte mac[] = {0x90, 0xA2, 0xDA, 0x10, 0x3A, 0xED};
+
+//Metic for each plant
+String name = "plant1";
 
 //Server addresses
-IPAddress serverAddress = (169, 254, 72, 168);
-int serverPort = 8080;
+//IPAddress server(192,168,2,61);
+char server[] = "donald.sch.bme.hu";
+int serverPort = 10305;
 
 //PIN data
 int lightDataPin = A5;
@@ -16,70 +18,112 @@ int moistureDataPin = A4;
 
 //Data that will be sent to server
 int moistureData = 0;
-int lightData = 0;    
+int lightData = 0;
 
-//UDP config
-EthernetUDP udp;
-unsigned int localPort = 8888; 
+//POST data
+String PostData = "[]";
 
-//Message buffers
-String formattedMessage;
-char messageBuffer[30]; 
-//char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
-
-
+//Ethernet client
+EthernetClient client;
 
 void setup()
 {
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect.
-  }
+  //while (!Serial) {
+    //; // wait for serial port to connect.
+  //}
   Serial.println("Serial monitor started!");
-  //Init
-  Ethernet.begin(mac, ip);
-  Serial.println("Successful ehernet initalization!");
-  udp.begin(localPort);
-  Serial.println("UDP port activated for communication"); //TODO write localport
+
+  Serial.println("Trying to get an IP address using DHCP");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // initialize the Ethernet device not using DHCP:
+    IPAddress ip = Ethernet.localIP();
+    Ethernet.begin(mac, ip);
+    Serial.print("My IP address: "+ ip);
+  }
+
+  //Give the Ethernet shield a second to initialize:
+  delay(1000);
+
+  //If you get a connection, report back via serial:
+  if (client.connect(server, serverPort)) {
+    Serial.println("Successful connection\n\n");
+    
+  } else {
+    //If you didn't get a connection to the server:
+    Serial.println("Connection failed\n\n");
+    //Waiting for restart
+    while(1){}
+  }
+  
 }
 
 void loop()
 {
-  // if there's data available, read a packet
-  /*int packetSize = Udp.parsePacket();
-  if (packetSize)
-  {
-    Serial.print("Received packet of size ");
-    Serial.println(packetSize);
-    Serial.print("From ");
-    IPAddress remote = Udp.remoteIP();
-    for (int i = 0; i < 4; i++)
-    {
-      Serial.print(remote[i], DEC);
-      if (i < 3)
-      {
-        Serial.print(".");
-      }
-    }
-    Serial.print(", port ");
-    Serial.println(Udp.remotePort());
-
-    // read the packet into packetBufffer
-    Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
-    Serial.println("Contents:");
-    Serial.println(packetBuffer);*/
-
-  
   moistureData = analogRead(moistureDataPin);
   lightData = analogRead(lightDataPin);
 
-  //Fill messageBuffer
-  formattedMessage = String("light : " + String(lightData) + " moisture :" + String(moistureData));
-  formattedMessage.toCharArray(messageBuffer, 30);
+  //Fill PostData
+  //(Only for light now.)
+  //Metric is light/moisutre + plant name
+  //Timestamp always -1  -> Graphite calculate it from package arrive time
+  String metric = "\"data.light." + name + "\"";
+  PostData = String("[{\"metric\": " + metric + ", \"value\": " + String(lightData) + ", \"timestamp\": -1" + "}]");
+  int inChar;
 
-  udp.beginPacket(serverAddress, serverPort);
-  udp.write(messageBuffer);
-  udp.endPacket();
-  Serial.println(messageBuffer);
-  delay(3000);
+  //Setup post request
+  if (client.connect(server, serverPort)) {
+  // Make a HTTP POST request:
+  //Route is /publish/gardener
+  client.println("POST /publish/gardener HTTP/1.0");
+  client.println("Host: " + String(server));
+  client.println("User-Agent: Arduino/1.0");
+  client.println("Connection: close");
+  //JSON message format
+  client.println("Content-Type: application/json;");
+  client.print("Content-Length: ");
+  client.println(PostData.length());
+  client.println();
+  client.println(PostData);
+
+  //Info in serail monitor too
+  Serial.println("The next information about plant:");
+  Serial.println(PostData);
+  Serial.println("is sent to server.\n");
+  } else {
+    //If we didn't get a connection to the server:
+    Serial.println("Connection failed");
+  }
+
+  //TODO 
+  //Ide jön a response ami a majd a konstansokat küldi a tresholdhoz :D
+  //Teszt fázis alatt.
+  int connectLoop = 0;
+
+  while(client.connected())
+  {
+    while(client.available())
+    {
+      inChar = client.read();
+      Serial.println("Response: ");
+      Serial.write(inChar);
+      connectLoop = 0;
+    }
+
+    delay(1);
+    connectLoop++;
+    if(connectLoop > 10000)
+    {
+      Serial.println();
+      Serial.println("Timeout");
+      client.stop();
+    }
+  }
+
+  Serial.println();
+  Serial.println("Teszt üzenet a response-hoz");
+  client.stop();
+
+  delay(30000);
 }
